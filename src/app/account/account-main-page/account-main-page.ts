3e1b9693840d5b57b6e6d2d93928670199322f80
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-09-22 07:17:06 +0200

    update account main

--- src/app/account/account-main-page/account-main-page.ts
+++ src/app/account/account-main-page/account-main-page.ts
@@ -1,19 +1,23 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { AuthenticationApi } from '../../api/authentication/authentication-api';
 import { User } from '../../shared/entities';
+import { PictureApi } from '../../api/pictures/picture-api';
+import { PicturesList } from "../../pictures-page/pictures-list/pictures-list";
 
 @Component({
   selector: 'app-account-main-page',
-  imports: [],
+  imports: [PicturesList],
   templateUrl: './account-main-page.html',
   styleUrl: './account-main-page.css'
 })
 export class AccountMainPage {
 
   protected readonly auth = inject(AuthenticationApi);
-  
-  constructor() {
-    console.log(this.auth.user());
-  }
+  private readonly pictureApi = inject(PictureApi);
+  readonly userId = signal<number>(this.auth.user()!.id);
+
+  protected readonly page = input(1, {transform: (val) => val ? Number(val):1});
+
+  protected readonly picturePage = this.pictureApi.getAllByUser(this.userId, this.page);
 
 }
