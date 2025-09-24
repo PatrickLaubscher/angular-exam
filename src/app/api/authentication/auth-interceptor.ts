5299dd1667605f4616f4c6e47dd5cec3ac75e4e8
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-09-24 10:40:31 +0200

    update authentication api

--- src/app/api/authentication/auth-interceptor.ts
+++ src/app/api/authentication/auth-interceptor.ts
@@ -1,13 +1,62 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { AuthenticationApi } from './authentication-api';
+import { catchError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
-  const clone = req.clone({
+
+  const auth = inject(AuthenticationApi);
+
+  const platformId = inject(PLATFORM_ID);
+  if(platformId!='browser' /* || !localStorage.getItem('token') || req.url.includes('refresh-token')*/) {
+    return next(req);
+  }
+
+  return next(cloneWithBearer(req)).pipe(
+    catchError(err => {
+      if(err.status == 403) {
+
+        auth.reRouteToLogin();
+
+        /*return auth.refreshToken().pipe(
+          concatMap(() => next(cloneWithBearer(req)))
+        );*/
+
+      };
+      throw err;
+    })
+  );
+   
+
+
+  function cloneWithBearer(req:HttpRequest<unknown>) {
+    const token = JSON.parse(localStorage.getItem('csrfToken') || '{}');
+    return req.clone({
+      setHeaders: {
+        "Content-Type": "application/json",
+        "X-XSRF-TOKEN": token
+      }
+    });
+  }
+
+
+
+/*   function cloneWithBearer(req:HttpRequest<unknown>) {
+    return req.clone({
+      setHeaders: {
+        'Authorization' : 'Bearer' + localStorage.getItem('token')
+      }
+    });
+  }
+ */
+// without refresh token
+/*   const clone = req.clone({
     setHeaders : {
       'X-Requested-With': 'XMLHttpRequest'
     },
     withCredentials: true
   })
   return next(clone);
-
+ */
 };
--- src/app/api/authentication/authentication-api.ts
+++ src/app/api/authentication/authentication-api.ts
@@ -1,9 +1,11 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
 import { User } from '../../shared/entities';
-import { tap } from 'rxjs';
+import { catchError, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { LoginCredentialsDTO, UserRegisterDTO } from '../dto';
 import { environment } from '../../../environments/environment.development';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginCredentialsDTO, UserRegisterDTO } from '../dto';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,10 @@ export class AuthenticationApi {
 
   private readonly http = inject(HttpClient);
   readonly user = signal<User|null>(null);
+  private readonly platformId = inject(PLATFORM_ID);
+  private readonly router = inject(Router);
+  private readonly snackBar = inject(MatSnackBar);
+
 
 
   constructor() {
@@ -23,12 +29,29 @@ export class AuthenticationApi {
       }
   }
 
-  register(dto:UserRegisterDTO) {
-    return this.http.post<User>(environment.serverUrl + '/api/user', dto);
+  register(credentials:UserRegisterDTO) {
+    return this.http.post<User>(environment.serverUrl + '/api/user', credentials);
   }
-
   
-  login(credentials: LoginCredentialsDTO) {
+
+  login(credentials:LoginCredentialsDTO) {
+    return this.http.get<User>(environment.serverUrl + '/api/account', {
+      headers: {
+        'Authorization': 'Basic ' + btoa(credentials.email + ':' + credentials.password)
+      },
+      withCredentials: true
+    })
+      .pipe(
+        tap(res => {
+          if(res) {
+            localStorage.setItem('user', JSON.stringify(res));
+            this.user.set(res);
+          }
+        })
+      );
+  }
+  
+/*   login(credentials: LoginCredentialsDTO) {
     return this.http.get<User>(environment.serverUrl + '/api/account', {
       headers: {
         'Authorization': 'Basic ' + btoa(credentials.email + ':' + credentials.password)
@@ -37,12 +60,34 @@ export class AuthenticationApi {
     }).pipe(
       tap(res => {
         localStorage.setItem('user', JSON.stringify(res));
-        console.log(res);
         this.user.set(res);
       })
     );
+  } */
+
+  reRouteToLogin() {
+    this.logout();
+    this.router.navigate(['login']);
+    this.snackBar.open('Votre session a expiré, connectez-vous à nouveau', 'ok', {duration: 5000});
   }
 
+
+// no refresh for the moment
+/*   refreshToken() {
+    return this.http.post<{message:string}>('/api/refresh-token', null, {withCredentials: true})
+      .pipe(
+        tap(res => localStorage.setItem('token', res.message)),
+        catchError(err => {
+          if(err.status == 403) {
+            this.logout();
+            this.router.navigate(['register']);
+            this.snackBar.open('Your session has expired, please login again', 'ok', {duration: 5000});
+          }
+        throw err;
+      })
+    );
+  }
+ */
 
   logout() {
     this.user.set(null);
@@ -53,6 +98,19 @@ export class AuthenticationApi {
   get isLogged() {
     return this.user() !== null;
   }
+
+
+/*   private getCookie(name: string): string | null {
+    const nameEQ = name + '=';
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i].trim();
+      if (c.indexOf(nameEQ) === 0) {
+        return c.substring(nameEQ.length);
+      }
+    }
+    return null;
+  } */
   
 }
 
